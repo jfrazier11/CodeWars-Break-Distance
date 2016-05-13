a19dde768ded739a6ca9d9fc26c75df26dac2602
Jeremi Frazier <jerm_803@yahoo.com>
2016-05-13 15:42:17 -0400

    Update Braking.java

--- Braking.java
+++ Braking.java
@@ -1,9 +1,26 @@
 import java.lang.Math;
+/**
+ * This class mimics brakes
+ * @author Jeremi Frazier  
+ * @version 1.0
+ */
 public class Braking {
-    
+   /**
+    * This method takes the speed of the the car and the total stoping distance formula to calcuate the distance need to brake
+    * @param v - the velocity
+    * @param mu - the reaction time of the driver
+    * @return double -the distance
+    */
+   
     public static double dist(double v, double mu) {    // suppose reaction time is 1
         v = v/((double)3600/1000);
         return ((v*v) / ((double)2*mu*9.81)) + v;
+   /**
+    * This method takes the distance of the the car and the total stoping distance formula to calcuate the speed the car was going
+    * @param v - the braking distance
+    * @param mu - the reaction time of the driver
+    * @return double -the speed
+    */
         
     }
     public static double speed(double d, double mu) {   // suppose reaction time is 1
